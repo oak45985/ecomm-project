703d19516016825fb8d33c106cc2e86594ad1d12
oak45985 <oak45985@gmail.com>
2022-09-28 06:35:11 -0500

    connection file established

--- config/connection.js
+++ config/connection.js
@@ -10,7 +10,7 @@ require('dotenv').config();
 
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, /*pw*/, {
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
       host: 'localhost',
       dialect: 'mysql',
       dialectOptions: {
